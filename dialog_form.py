4d764d1751efcb5645c5a474b894c4920c818ee2
Frederick Morrison <frederick.morrison@outlook.com>
2024-07-03 16:23:39 -0600

    Add textbox and button to dialog form

--- dialog_form.py
+++ dialog_form.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QDialog
+from PySide6.QtWidgets import QDialog, QLineEdit, QPushButton, QVBoxLayout
 
 
 class DialogForm(QDialog):
@@ -6,3 +6,18 @@ class DialogForm(QDialog):
     def __init__(self, parent=None):
         super(DialogForm, self).__init__(parent)
         self.setWindowTitle("Example PySide6 Dialog Form")
+        # Create widgets
+        self.edit = QLineEdit("Write my name here..")
+        self.button = QPushButton("Show Greetings")
+        # Create layout and add widgets
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.edit)
+        layout.addWidget(self.button)
+        # Set dialog layout
+        self.setLayout(layout)
+        # Add button signal (i.g., event listener) to greetings slot
+        self.button.clicked.connect(self.greetings)
+
+# Greets the user
+    def greetings(self):
+        print(f"Hello {self.edit.text()}")
