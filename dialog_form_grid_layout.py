6dedd05f78beb3865861fc41d8386c31fb615027
Frederick Morrison <frederick.morrison@outlook.com>
2024-07-03 18:27:02 -0600

    add address and age sections to grid layout example

--- dialog_form_grid_layout.py
+++ dialog_form_grid_layout.py
@@ -16,12 +16,32 @@ class DialogFormGridLayout(QDialog):
         self.setFixedSize(QSize(dialog_width, dialog_height))
 
         self.form_layout = QFormLayout()
+
+        # Section 1 Name label and text box
         self.horizontal_layout1 = QHBoxLayout()
 
-        self.label1 = QLabel('Name')
-        self.input1 = QLineEdit()
+        self.name_label = QLabel('Name')
+        self.name_input = QLineEdit()
 
-        self.horizontal_layout1.addWidget(self.label1)
-        self.horizontal_layout1.addWidget(self.input1)
+        self.horizontal_layout1.addWidget(self.name_label)
+        self.horizontal_layout1.addWidget(self.name_input)
 
         self.form_layout.addRow(self.horizontal_layout1)
+
+        # Section 2 Address and Age input
+        self.vertical_layout2 = QVBoxLayout()
+
+        self.address_label = QLabel('Address')
+        self.address_input = QLineEdit()
+
+        self.age_label = QLabel('Age')
+        self.age_input = QLineEdit()
+
+        # add lables and corresponding input areas in the correct order
+        for item in [self.address_label, self.address_input, self.age_label, self.age_input]:
+            self.vertical_layout2.addWidget(item)
+
+        self.form_layout.addRow(self.vertical_layout2)
+
+        # Set dialog layout
+        self.setLayout(self.form_layout)
